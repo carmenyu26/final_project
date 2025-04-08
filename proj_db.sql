1a754441358f2733d4b3911f0a34c13879a6f38f
carmeny26 <yu.carm@northeastern.edu>
2025-04-08 01:07:02 -0400

    loaded reaction data into mysql tables

---
+++ proj_db.sql
@@ -0,0 +1,43 @@
+-- CREATE DATABASE AND TABLES
+-- LOAD DATA THROUGH PYTHON
+
+-- ----- Create Database -----
+DROP DATABASE IF EXISTS metabolic_pathways;
+CREATE DATABASE metabolic_pathways;
+
+USE metabolic_pathways;
+SHOW TABLES;
+
+
+-- ------ Create Tables ------
+
+-- Organism
+-- DROP TABLE IF EXISTS organism;
+-- CREATE TABLE organism (
+-- 	organism_id INT PRIMARY KEY AUTO_INCREMENT,
+--     organism_name VARCHAR(55),
+--     taxonomy VARCHAR(55)
+-- );
+
+-- Reactions
+-- DROP TABLE IF EXISTS reaction;
+-- CREATE TABLE reaction (
+-- 	reaction_id INT PRIMARY KEY,
+--     reaction_name VARCHAR(55) NOT NULL,
+--     lower_bound INT,
+--     upper_bound INT,
+--     is_reversible TINYINT
+-- );
+
+CREATE TABLE reaction (
+	id VARCHAR(55),
+    name VARCHAR(55),
+    lower_bound INT,
+    upper_bound INT,
+    objective_coefficient INT
+);
+
+
+DROP TABLE reaction;
+SELECT * FROM reaction;
+SELECT COUNT(*) FROM reaction;
