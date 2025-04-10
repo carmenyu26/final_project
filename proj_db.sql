03b610070eca9957261276754bfd5018a2d972dc
sarahshentu <utnehs.sarah@gmail.com>
2025-04-10 17:24:14 -0400

    Add files via upload

--- proj_db.sql
+++ proj_db.sql
@@ -12,29 +12,41 @@ SHOW TABLES;
 -- ------ Create Tables ------
 
 -- Organism
--- DROP TABLE IF EXISTS organism;
--- CREATE TABLE organism (
--- 	organism_id INT PRIMARY KEY AUTO_INCREMENT,
---     organism_name VARCHAR(55),
---     taxonomy VARCHAR(55)
--- );
+ DROP TABLE IF EXISTS organism;
+ CREATE TABLE organism (
+ 	organism_id INT PRIMARY KEY AUTO_INCREMENT,
+     organism_name VARCHAR(55),
+     taxonomy VARCHAR(55)
+ );
 
 -- Reactions
--- DROP TABLE IF EXISTS reaction;
--- CREATE TABLE reaction (
--- 	reaction_id INT PRIMARY KEY,
---     reaction_name VARCHAR(55) NOT NULL,
---     lower_bound INT,
---     upper_bound INT,
---     is_reversible TINYINT
--- );
+ DROP TABLE IF EXISTS reaction;
+ CREATE TABLE reaction (
+ 	reaction_id INT PRIMARY KEY,
+     reaction_name VARCHAR(55) NOT NULL,
+     lower_bound INT,
+     upper_bound INT,
+     is_reversible TINYINT
+ );
+ 
+-- Genes
+ DROP TABLE IF EXISTS gene;
+ CREATE TABLE gene (
+	gene_id INT PRIMARY KEY,
+    organism_id INT,
+    gene_name VARCHAR(45),
+    gene_function VARCHAR(45),
+    CONSTRAINT gene_organism_fk FOREIGN KEY (organism_id) references organism (organism_id)
+);
 
-CREATE TABLE reaction (
-	id VARCHAR(55),
-    name VARCHAR(55),
-    lower_bound INT,
-    upper_bound INT,
-    objective_coefficient INT
+-- Metabolite
+DROP TABLE IF EXISTS metabolite;
+CREATE TABLE metabolite (
+	metabolite_id INT PRIMARY KEY,
+    metabolite_name VARCHAR(45),
+    formula VARCHAR(45),
+    charge VARCHAR(45),
+    compartment VARCHAR(45)
 );
 
 -- check reaction table
